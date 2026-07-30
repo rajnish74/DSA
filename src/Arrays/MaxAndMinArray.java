6a044cdfd6099d9a6b865ff1781aee7394dbe913
rajnish <rajnishk748193@gmail.com>
2026-07-30 10:43:44 +0530

    Implement maximum and minimum element search in an array

---
+++ src/Arrays/MaxAndMinArray.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class MaxAndMinArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(max+" "+min);
+
+    }
+}
